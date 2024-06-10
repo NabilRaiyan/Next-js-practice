274f3479f16945fe96abd3a5ac3e7c0e4a254444
NabilRaiyan <rraiyan77@gmail.com>
2024-06-10 20:07:11 +0600

    creating get method

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,14 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/add')
+  getAdd(): number {
+    return this.appService.getAddition();
+  }
+
+  @Get('/user')
+  getUser(): object {
+    return this.appService.getUser();
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,16 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getAddition(): number {
+    return 3 + 5;
+  }
+
+  getUser(): object {
+    return {
+      name: 'Raiyan Al Sultan',
+      age: 24,
+      gender: 'Male',
+    };
+  }
 }
