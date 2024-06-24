07b5494858daca293cbf5f8710db40cddd2816a6
NabilRaiyan <rraiyan77@gmail.com>
2024-06-24 22:23:04 +0600

    adding new route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,6 +2,7 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
+// 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
