d26218aa6ff6802d355cecfd6b5227c5a99fa94b
NabilRaiyan <rraiyan77@gmail.com>
2024-06-27 20:37:47 +0600

    creating admin table entity

---
+++ src/admin/admin.entity.ts
@@ -0,0 +1,15 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('admin')
+export class AdminEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  name: string;
+  @Column()
+  email: string;
+  @Column()
+  password: string;
+  @Column()
+  gender: string;
+}
