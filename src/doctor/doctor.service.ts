32269442d204e51575abdd7ccc82dd7bcb0af049
NabilRaiyan <rraiyan77@gmail.com>
2024-06-27 19:48:02 +0600

    adding method for CRUD doctor

--- src/doctor/doctor.service.ts
+++ src/doctor/doctor.service.ts
@@ -1,55 +1,60 @@
 /* eslint-disable prettier/prettier */
 import { Injectable } from "@nestjs/common";
 import { DoctorDto } from "./doctor.dto";
+import { Repository } from "typeorm";
+import { DoctorEntity } from "./doctor.entity";
+import { InjectRepository } from "@nestjs/typeorm";
 
 // doctor service class
 @Injectable()
 export class DoctorService{
-    // get all doctor
-    getDoctor(): object {
-        return {
-            Doctors: {
-                doc1:"Raiyan",
-                doc2:"Sultan",
-                doc3:"Jhon",
-                doc4:"Abraham",
-                doc5:"Yousuf"
-            }
-        };
-    }
+    constructor(@InjectRepository(DoctorEntity) private readonly doctorRepository: Repository<DoctorEntity>){}
 
-    // get doctor by name
-    getDocByName(docName: string): object{
-        return {
-            id: 1,
-            name: docName,
-            gender: "Male"
+    // // get all doctor
+    // getDoctor(): object {
+    //     return {
+    //         Doctors: {
+    //             doc1:"Raiyan",
+    //             doc2:"Sultan",
+    //             doc3:"Jhon",
+    //             doc4:"Abraham",
+    //             doc5:"Yousuf"
+    //         }
+    //     };
+    // }
+
+    // // get doctor by name
+    // getDocByName(docName: string): object{
+    //     return {
+    //         id: 1,
+    //         name: docName,
+    //         gender: "Male"
             
-        }
-    }
-    // add new doctor
-    addDoctor(myObj: DoctorDto): object{
-        console.log(myObj);
-        return myObj;
-    }
+    //     }
+    // }
+    // // add new doctor
+    // addDoctor(myObj: DoctorDto): object{
+    //     console.log(myObj);
+    //     return myObj;
+    // }
 
-    // update doctor information method
-    updateDocInfo(myObj: object):object{
-        console.log(myObj);
-        return myObj;
-    }
+    // // update doctor information method
+    // updateDocInfo(myObj: object):object{
+    //     console.log(myObj);
+    //     return myObj;
+    // }
 
-    // update password method
-    updatePassword(Userpassword: object, id:number): object{
-        return {message: "Password is Updated ", id,   body:Userpassword};
-    }
+    // // update password method
+    // updatePassword(Userpassword: object, id:number): object{
+    //     return {message: "Password is Updated ", id,   body:Userpassword};
+    // }
 
-    // delete cotor by id method
-    deleteDocById(id: number): object{
-        return {
-            message: "Successfully deleted the doctor",
-            id: id,
-        }
-    }
+    // // delete cotor by id method
+    // deleteDocById(id: number): object{
+    //     return {
+    //         message: "Successfully deleted the doctor",
+    //         id: id,
+    //     }
+    // }
 
 }
