e567446ec0abead35d9380295751d26824800ee0
NabilRaiyan <rraiyan77@gmail.com>
2024-09-16 15:22:56 +0600

    creating project

--- client/app/page.js
+++ client/app/page.js
@@ -1,10 +1,7 @@
 import Image from "next/image";
-import Nav from "./components/Nav";
 
 export default function Home() {
   return (
-    <>
-    <Nav />
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
@@ -100,6 +97,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-    </>
   );
 }
