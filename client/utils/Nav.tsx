9ce8fc29a0fcf943a946b3029643703c7e79190d
NabilRaiyan <rraiyan77@gmail.com>
2024-09-17 11:08:47 +0600

    creating form and home page

--- client/utils/Nav.tsx
+++ client/utils/Nav.tsx
@@ -8,7 +8,7 @@ const Nav = () => {
     <div className='grid grid-cols-3 gap-2'>
         <div className="logo">
             <Link className='' href="/home">
-            <Image alt='logo' width={400} height={40} src="/next.png" className='rounded mt-4 self-center'></Image>
+            <Image alt='logo' width={38} height={40} src="/next.png" className='rounded mt-4 self-center'></Image>
             </Link>
         </div>
         <Link href="/">Home</Link>
