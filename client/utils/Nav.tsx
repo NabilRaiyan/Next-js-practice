d050e43c8ec674944b02eea4645298ac17126490
NabilRaiyan <rraiyan77@gmail.com>
2024-09-17 11:10:56 +0600

    creating form and home page

--- client/utils/Nav.tsx
+++ client/utils/Nav.tsx
@@ -5,14 +5,13 @@ import Image from 'next/image'
 const Nav = () => {
   return (
     <>
-    <div className='grid grid-cols-3 gap-2'>
+    <div className='grid grid-cols-3'>
         <div className="logo">
             <Link className='' href="/home">
             <Image alt='logo' width={38} height={40} src="/next.png" className='rounded mt-4 self-center'></Image>
             </Link>
         </div>
         <Link href="/">Home</Link>
-        <Link href="/">About</Link>
         <Link href="/form">Form</Link>
 
 
