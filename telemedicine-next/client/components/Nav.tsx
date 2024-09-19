16acc201518a6276cb421e94eb06e15204afedde
NabilRaiyan <rraiyan77@gmail.com>
2024-09-19 15:58:26 +0600

    creatign navbar

---
+++ telemedicine-next/client/components/Nav.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Nav() {
+  return (
+    <div className='text-gray-900 text-lg font-serif gap-2 bg-blue-100 m-4 rounded'>
+      <nav className='flex justify-center gap-[60px] p-3'>
+        <div className='flex flex-col-2 gap-2 mt-2'>
+            <Image className='rounded ml-4' src={'/logo.png'} alt='logo' width={40} height={40} />
+            <p className='mt-2 text-xl font-thin text-gray-800'>Doc<span className='font-serif text-orange-500'>Line</span></p>
+        </div>
+        <Link className='mt-3 hover:underline hover:text-gray-500' href="#">Home</Link>
+        <Link className='mt-3 hover:underline hover:text-gray-500' href="#">About</Link>
+        <Link className='mt-3 hover:underline hover:text-gray-500' href="#">Contact</Link>
+        <Link className='mt-1 bg-blue-600 p-1 rounded px-2 text-white hover:text-black hover:bg-orange-50' href="#">Sign In</Link>
+      </nav>
+    </div>
+  )
+}
