11ffaabe8b4fca5668af53ac00bf4053d0f69594
NabilRaiyan <rraiyan77@gmail.com>
2024-09-22 22:15:21 +0600

    creatign react hook form

--- telemedicine-next/client/app/auth/login/page.tsx
+++ telemedicine-next/client/app/auth/login/page.tsx
@@ -56,7 +56,6 @@ export default function Login() {
           })
         }>
           <input {...register('email')} placeholder='Enter email' />
-
           <input {...register('password')} placeholder='Enter password' type='password' />
           <input type='submit' />
 
