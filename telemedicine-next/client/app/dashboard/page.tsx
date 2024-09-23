5e434664bd45c1d609b997fbee7a1afae634ec8e
NabilRaiyan <rraiyan77@gmail.com>
2024-09-23 15:55:22 +0600

    creating login and dashboard

--- telemedicine-next/client/app/dashboard/page.tsx
+++ telemedicine-next/client/app/dashboard/page.tsx
@@ -1,63 +1,9 @@
-"use client";
 
-import React from 'react'
-import { Metadata } from 'next';
-import Nav from '@/components/Nav';
-import axios from 'axios';
+import Dashboard from "@/components/Dashboard";
 
+export default function DashboardPage(){
 
-// export const metadata: Metadata = {
-//     title: 'DocLine || Dashboard',
-//     description:
-//       'My Dashboard',
-//   };
-
-// dashboard
-export default function page() {
-
-  // const [appointmentData, setAppointmentData] = useState<Appointment[]>([]); // Initialize as an empty array
-
-  //   // getting all appointment for doctor   
-  // async function getAppointment() {
-  //   try {
-  //     const appResponse = await axios.get('http://localhost:3000/appointments/appointmentHistory', {
-  //       withCredentials: true, // This ensures cookies are sent with the request
-  //       params: { doctorId },
-  //     });
-  //     setAppointmentData(appResponse.data);
-  //     console.log(appResponse.data); // Log the fetched appointment data
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }
-
-
-//   // create prescription for patient
-//   async function createPrescription(){
-//     try{
-//         const prescriptionData = {
-//             prescription_details: "New Data for prescription",
-//         }
-//         const response = await axios.post('http://localhost:3000/prescription/createPrescription/3', prescriptionData, {
-//             withCredentials: true,
-//             params: doctorId
-//         });
-//         console.log(response)
-
-//     }
-//     catch(error){
-//         console.log(error)
-//     }
-// }
-
-
-    
-  return (
-    <div className='flex flex-col gap-2'>
-      <div>
-        <Nav />
-        <button>Login</button>
-      </div>
-    </div>
+  return(
+    <Dashboard/>
   )
 }
---
+++ telemedicine-next/client/components/Login.tsx
@@ -0,0 +1,100 @@
+"use client";
+
+import React, { useState } from 'react';
+import {useForm} from 'react-hook-form';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+
+import axios from 'axios';
+export default function Login() {
+
+  const router = useRouter();
+
+  const {register, handleSubmit, formState: {errors}, reset} = useForm({
+    defaultValues:
+    {
+      email: "",
+      password: ""
+    }
+  });
+
+  // doctor interface
+  interface Doctor {
+    d_id: string;
+    d_name: string;
+    d_chamber_address: string;
+    d_specialize: string;
+  }
+
+  // appointment interface
+  interface Appointment {
+    appointment_id: string;
+    appointment_status: string;
+    consultation_notes: string;
+  }
+
+  interface loginInputObj{
+    email: string,
+    password: string
+
+  }
+
+  const [doctorData, setDoctorData] = useState<Doctor | null>(null);
+  const [doctorId, setDoctorId] = useState<string | null>(null);
+  
+
+    // handle login   
+  async function handleLogin(loginInput: loginInputObj) {
+    if(loginInput){
+      try {
+        const loginData = {
+          u_email: loginInput.email,
+          u_password: loginInput.password,
+        };
+        const response = await axios.post('http://localhost:3000/users/login', loginData, {
+          withCredentials: true, // Include credentials in login request
+        });
+        const data = response.data.doctor;
+        setDoctorData(data);
+        setDoctorId(data.d_id);
+        console.log(doctorId);
+        console.log(doctorData);
+        if(doctorId !== null){
+          router.push(`/dashboard?docId=${doctorId}`)
+        }
+        
+      } catch (error) {
+        console.log(error);
+      }
+    }else{
+      console.log("Login info is null")
+    }
+    
+  }
+
+
+  const onSubmit = (data: loginInputObj) => {
+    handleLogin(data); 
+  };
+
+
+  // rendering doc info
+  return (
+    <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input {...register("email", {required: "Please enter email", 
+                              pattern: {value: /^[A-Za-z0-9._%+-]+@gmail\.com$/,
+                                message: "Please enter a valid email address"
+                              }
+                              })} placeholder='Enter email' />
+            <p>{errors && errors.email?.message}</p>
+          <input {...register("password", {required: "Please enter password"})} 
+                              placeholder='Enter password' type='password' />
+          <p>{errors && errors.password?.message}</p>
+
+          <button type='submit'>Login</button>
+
+        </form>
+    </div>
+  );
+}
