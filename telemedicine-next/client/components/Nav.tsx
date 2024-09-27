3f5245fb3e0c7d1ed2cf8f9f00c7cc04e66b3f7f
NabilRaiyan <rraiyan77@gmail.com>
2024-09-27 22:28:26 +0600

    creating dashboard

--- telemedicine-next/client/components/Nav.tsx
+++ telemedicine-next/client/components/Nav.tsx
@@ -28,10 +28,8 @@ export default function Nav({loggedInState}: NavProps) {
            <Link className='mt-3 hover:underline hover:text-gray-500' href="/">Home</Link>
            <Link className='mt-3 hover:underline hover:text-gray-500' href="#">About</Link>
            <Link className='mt-1 bg-blue-600 p-1 rounded px-3 text-white hover:text-black hover:bg-orange-50' href="/auth/login">Sign In</Link>
-
           </>
       )}
       </nav>
-      
   )
 }
